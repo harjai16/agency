87b2e0f3b6ea5a8a1a2c8cfffb0850b3fc71520b
harjai16 <ashwanik2198@gmail.com>
2025-12-24 15:08:41 +0530

    ashwani

--- src/componenets/Hero.jsx
+++ src/componenets/Hero.jsx
@@ -90,7 +90,7 @@ const Hero = () => {
           </motion.div>
 
           {/* Heading */}
-          <motion.h1
+          <motion.h1    
             initial={{ opacity: 0, y: 16 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5, delay: 0.05 }}
