81aa00c9229909c250c7a96e4f473571ba3912e5
harjai16 <ashwanik2198@gmail.com>
2026-01-02 00:42:39 +0530

    logo

--- src/componenets/Contact.jsx
+++ src/componenets/Contact.jsx
@@ -3,7 +3,7 @@
 import React from "react";
 import { motion } from "framer-motion";
 import Section from "./ui/Section";
-import ContactForm from "@/componenets/global/ContactForm";    
+import ContactForm from "@/componenets/global/ContactForm";       
 
 const Contact = ({ pageName = "Home" }) => {
   return (
